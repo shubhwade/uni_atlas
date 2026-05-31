8bf77818d848b8573e337455eb3a78466521463f
shubhwade <shubhwade3@gmail.com>
2026-05-31 13:22:17 +0530

    chore: add debug try-catch to api/index.js to surface startup errors

--- api/index.js
+++ api/index.js
@@ -3,6 +3,19 @@ const path = require("path");
 // Load environment variables from the nested app when present.
 require("dotenv").config({ path: path.join(__dirname, "../abroadready/.env") });
 
-const { app } = require("../abroadready/server");
+let app;
+try {
+  app = require("../abroadready/server").app;
+} catch (err) {
+  const express = require("express");
+  app = express();
+  app.all("*", (req, res) => {
+    res.status(500).json({
+      error: "Vercel Initialization Crash",
+      message: err.message,
+      stack: err.stack,
+    });
+  });
+}
 
 module.exports = app;
