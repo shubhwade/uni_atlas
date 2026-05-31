cee3f731a46fd91a2c52de14b31e9df2ae45c0ab
shubhwade <shubhwade3@gmail.com>
2026-05-31 13:48:35 +0530

    fix: downgrade pdf-parse to v1.1.1 and remove DOMMatrix polyfills to resolve Vercel crash

--- abroadready/lib/resumeAnalyzer.js
+++ abroadready/lib/resumeAnalyzer.js
@@ -1,15 +1,6 @@
 const fs = require("fs");
 const path = require("path");
 const fetch = require("node-fetch");
-// Polyfill DOMMatrix for pdf-parse in Node 18+ environments like Vercel
-if (typeof global.DOMMatrix === "undefined") {
-  global.DOMMatrix = class DOMMatrix {
-    constructor() { this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0; }
-  };
-}
-if (typeof globalThis.DOMMatrix === "undefined") {
-  globalThis.DOMMatrix = global.DOMMatrix;
-}
 const { getDB } = require("../database/db");
 const cloudinary = require("./cloudinary");
 const affinda = require("./affinda");
--- api/index.js
+++ api/index.js
@@ -1,15 +1,3 @@
-// Polyfill DOMMatrix and DOMPoint for Vercel Node 18+ (required by pdf-parse)
-if (typeof global.DOMMatrix === "undefined") {
-  global.DOMMatrix = class DOMMatrix {
-    constructor() { this.a = 1; this.b = 0; this.c = 0; this.d = 1; this.e = 0; this.f = 0; }
-  };
-}
-if (typeof global.DOMPoint === "undefined") {
-  global.DOMPoint = class DOMPoint {
-    constructor(x = 0, y = 0, z = 0, w = 1) { this.x = x; this.y = y; this.z = z; this.w = w; }
-  };
-}
-
 const path = require("path");
 
 // Load environment variables from the nested app when present.
